5fd562494084528271f71edd7e657926618521c1
Thuyen Code <thuyencode@proton.me>
2025-03-15 15:14:16 +0700

    Install Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react()],
+  plugins: [deno(), react(), tailwindcss()]
 })
