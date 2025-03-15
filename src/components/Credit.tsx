112fa2bc4cb96d8065723b3a649bf9e683958c8a
Thuyen Code <thuyencode@proton.me>
2025-03-15 17:31:27 +0700

    Fix link

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -2,7 +2,7 @@ export const Credit = () => (
   <h1 className='text-xl font-semibold'>
     <a
       className='text-red-400 hover:underline'
-      href='https://uiverse.io/profile/AbanoubMagdy1'
+      href='https://uiverse.io/AbanoubMagdy1/fluffy-octopus-90'
       target='_blank'
     >
       Switch
